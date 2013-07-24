3bb04f7876b0940a2a44afe71349e677670a3a69
motley <motley.slate@gmail.com>
2013-07-24 18:08:20 -0500

    Force fast charge

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -51,6 +51,11 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1166,6 +1171,10 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 	if (force_fast_charge == 1) {
 		mA = USB_FASTCHG_LOAD;
 		pr_info("USB fast charging is ON - 1000mA.\n");
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+			mA = USB_FASTCHG_LOAD;
+			pr_info("USB fast charging is ON - 1000mA.\n");
 	} else {
 		pr_info("USB fast charging is OFF.\n");
 	}
