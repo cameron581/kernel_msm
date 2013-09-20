d0b034e732e7764bbfb46e5ba3dbdb67b7b18011
houst0nn <houstonn@gmail.com>
2013-09-20 18:40:45 +0000

    prima: wcnss: adapted from jb_3.2.1 caf to fix errors for building prima
    @thanks chadouming

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -561,7 +561,7 @@ int wcnss_wlan_iris_xo_mode(void)
 EXPORT_SYMBOL(wcnss_wlan_iris_xo_mode);
 
 
-static void wcnss_suspend_notify(void)
+void wcnss_suspend_notify(void)
 {
 	void __iomem *pmu_spare_reg;
 	u32 reg = 0;
@@ -577,7 +577,7 @@ static void wcnss_suspend_notify(void)
 	spin_unlock_irqrestore(&reg_spinlock, flags);
 }
 
-static void wcnss_resume_notify(void)
+void wcnss_resume_notify(void)
 {
 	void __iomem *pmu_spare_reg;
 	u32 reg = 0;
