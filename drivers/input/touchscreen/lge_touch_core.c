3a6f7bc4427e759c39cb057605fb3f4ca8eb1fcd
anarkia1976 <stefano.villa1976@gmail.com>
2013-11-26 17:38:16 +0100

    drivers: input: 2w: remove device_init_wakeup

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -1918,8 +1918,6 @@ static int touch_probe(struct i2c_client *client,
 		ts->accuracy_filter.touch_max_count = one_sec / 2;
 	}
 
-        device_init_wakeup(&client->dev, true);
-
 #if defined(CONFIG_HAS_EARLYSUSPEND)
 	ts->early_suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1;
 	ts->early_suspend.suspend = touch_early_suspend;
